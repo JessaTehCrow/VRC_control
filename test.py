8edfc424c87d9c66be7dd5b417ec5d9db2d9d054
JessaTehCrow <jellekok.3.4@gmail.com>
2023-11-21 01:33:13 +0100

    Clean root directory

--- test.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import time
-pygame.init()
-pygame.mixer.init()
-
-sound = pygame.mixer.Sound("close.mp3")
-sound.play()
-time.sleep(1)
--- test2.py
+++
@@ -1,9 +0,0 @@
-import websocket
-
-def error(*a):
-    print(a)
-
-
-while True:
-    app = websocket.WebSocketApp("ws://localhost:8080", on_error=error)
-    app.run_forever()
