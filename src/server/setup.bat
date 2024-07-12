656591d0d4537312b34c2d4739715cb416cd8d4b
JessaTehCrow <jellekok.3.4@gmail.com>
2024-07-12 06:15:44 +0200

    Wrong directory

--- src/server/setup.bat
+++ src/server/setup.bat
@@ -1,10 +1,10 @@
-if ! command -v php &> /dev/null
-then
-    echo "PHP not installed"
-    echo "Please instal PHP >= 8.2.15"
-fi
-
-cd src/server/src
+@ECHO off
+WHERE php >nul 2>nul
+IF %ERRORLEVEL% EQU 1 (
+    ECHO PHP not installed.
+    echo Please instal PHP ^>= 8.2.15
+    exit
+)
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -12,4 +12,5 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 php composer.phar validate
-rm composer.phar
+
+del composer.phar
--- src/server/setup.sh
+++ src/server/setup.sh
@@ -1,12 +1,9 @@
-@ECHO off
-WHERE php >nul 2>nul
-IF %ERRORLEVEL% EQU 1 (
-    ECHO PHP not installed.
-    echo Please instal PHP ^>= 8.2.15
-    exit
-)
-
-cd src/server/src
+#!/usr/bin/env bash
+if ! command -v php &> /dev/null
+then
+    echo "PHP not installed"
+    echo "Please instal PHP >= 8.2.15"
+fi
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -14,5 +11,4 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 php composer.phar validate
-
-del composer.phar
+rm composer.phar
